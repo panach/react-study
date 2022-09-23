54ad26eabb6c88aa3a9c1069fd3764df7d85f09a
panach <panach@deug-ui-MacBookPro.local>
2022-09-23 17:49:54 +0900

    2step

--- src/ExpenseItem/ExpenseItem.js
+++ src/ExpenseItem/ExpenseItem.js
@@ -1,15 +1,12 @@
 import "./ExpenseItem.css"
-function ExpenseItems () {
-    const ExpenseDate = new Date(2021, 2, 28);
-    const ExpenseTitle = 'anjflkjdsalsadflkadfs';
-    const ExpenseAmount = 3000000;
-
+function ExpenseItems (props) {
+    const month = props.date.toLocaleString('ko-KR', { month: 'long'});
     return (
         <>
             <h3 className="header">header</h3>
-            <p className="text">{ExpenseTitle}</p>
-            <p className="text">{ExpenseDate.toISOString()}</p>
-            <p className="text">{ExpenseAmount + 1 }</p>
+            <p className="text">{props.title}</p>
+            <p className="text">{props.amount}</p>
+            <p className="text">{month}</p>
         </>
     );
 };
