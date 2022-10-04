e50dbab6518880d9e65066b1ca7ff7134f49884a
panach <panach@deug-ui-MacBookPro.local>
2022-10-04 18:01:22 +0900

    51. usestate

--- src/conponenet/Expenses/ExpenseItem.js
+++ src/conponenet/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
+
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Cart from "../UI/cart";
-
 import "./ExpenseItem.css";
 
 function ExpenseItems (props) {
+    const [title, setTitle] = useState(props.title);
+    const clickHandler = () => {
+        setTitle('up!!!!');
+        console.log('run');
+    };
+
    return (
         <>
             <Cart>
-                <h3 className="header">header</h3>
+                <h3 className="header">#{title}</h3>
                 <p className="text">{props.title}</p>
                 <p className="text">{props.amount}</p>
                 <ExpenseDate date={props.date}/>
+                <button onClick={clickHandler}>button_BTN</button>
             </Cart>
         </>
     );
