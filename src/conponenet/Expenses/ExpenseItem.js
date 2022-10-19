3bd32665971cc7824520de525aaa0613f7975f8d
panach <panach@deug-ui-MacBookPro.local>
2022-10-19 17:59:49 +0900

    stat 사용법

--- src/conponenet/Expenses/ExpenseItem.js
+++ src/conponenet/Expenses/ExpenseItem.js
@@ -5,20 +5,55 @@ import Cart from "../UI/cart";
 import "./ExpenseItem.css";
 
 function ExpenseItems (props) {
-    const [title, setTitle] = useState(props.title);
-    const clickHandler = () => {
-        setTitle('up!!!!');
-        console.log('run');
+    // const [title, setTitle] = useState(props.title);
+
+    const [enterTitle, setEnterTitle] = useState('');
+    const [enterAmount, setAmount] = useState('');
+    const [enterDate, setDate] = useState('');
+    
+    // const [userInput, setUserInput] = useState({
+    //     enterTitle: '',
+    //     enterAmount: '',
+    //     enterDate: ''
+    // });
+    const titleClickHandler = (event) => {
+        setEnterTitle(event.target.value);
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enterTitle: event.target.value};
+        // });
+
+    };
+    const amountClickHandler = (event) => {
+        setAmount('event.target.value2');
+    };
+    const dateClickHandler = (event) => {
+        setDate('event.target.value3');
+    };
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseDate = {
+            title: enterTitle,
+            amount: enterAmount,
+            date: new Date(enterDate)
+        };
+
+        console.log(expenseDate);
     };
 
    return (
         <>
             <Cart>
-                <h3 className="header">#{title}</h3>
-                <p className="text">{props.title}</p>
-                <p className="text">{props.amount}</p>
+                <form onSubmit={submitHandler}>
+                <input type="text" onChange={titleClickHandler}></input>
+                <h3 className="header">#{enterTitle}</h3>
+                <p className="text">{enterDate}</p>
+                <p className="text">{enterAmount}</p>
                 <ExpenseDate date={props.date}/>
-                <button onClick={clickHandler}>button_BTN</button>
+                <button onClick={titleClickHandler}>button_BTN1</button>
+                <button onClick={amountClickHandler}>button_BTN2</button>
+                <button onClick={dateClickHandler}>button_BTN3</button>
+                </form>
             </Cart>
         </>
     );
